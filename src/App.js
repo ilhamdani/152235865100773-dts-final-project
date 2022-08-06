072f3c8685d5b2c581763b01e0f3c57301c2757d
tanidev-tech <ilhamdani@gmail.com>
2022-08-06 12:09:53 -0500

    add login form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Graduates from './components/Graduates';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './components/Login';
 
 function App() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Graduates />
-      </div>
+      {!user ? (<Login />) : (
+        <div className="app__body">
+          <Graduates />
+        </div>
+      )}
     </div>
   );
 }
